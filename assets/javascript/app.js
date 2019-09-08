07fbd7dcc387a7b43b5cfda59c0635d34d3cf223
Greg Hawthorne <ghawthorne@me.com>
2019-09-07 21:03:38 -0700

    timer switch js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 var gameState = {
 
     // set the time at 60 seconds, and count down by 1 second
-    timeRemaining: 660,
+    timeRemaining: 60,
 
     // start the timer, hide the start page, show the questions
     startTimer: function () {
